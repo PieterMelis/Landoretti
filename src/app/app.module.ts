ed5f5ab1f27eb7496790ecc4f87e15d49e71952f
pieter melis <pieter.melis@hotmail.com>
2017-12-10 22:13:55 +0100

    basic setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { routing } from './app.routes';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { OverzichtComponent } from './overzicht/overzicht.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OverzichtComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { OverzichtComponent } from './overzicht/overzicht.component';
+
+
+
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent}
+  {path: 'overzicht', component: OverzichtComponent}
+];
+
+export const routing = RouterModule.forRoot(routes);
